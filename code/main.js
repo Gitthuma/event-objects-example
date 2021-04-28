3a646cf452e349d5299c59726942225e44f68ef4
Gitthuma <george.githuma@gmail.com>
2021-04-28 08:00:10 +0300

    Created a function that takes a number as a parametre and genarates a random number from it

--- code/main.js
+++ code/main.js
@@ -4,3 +4,9 @@ for (i = 1; 1<= 16; i++) {
     const myDiv = document.createElement('div');
     document.body.appendChild(myDiv);
 }
+
+/*Create a function that takes a number as a parametre and genarates a random number from it*/
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
